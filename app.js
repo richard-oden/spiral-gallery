43c8dffd168f0d3b6eb32a8fea7711addbce0da5
Richard Oden <richard.thomas.oden@gmail.com>
2020-07-29 12:11:52 -0400

    Change image pop out to expand

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-function togglePopOut() {
-    if (event.target.tagName === 'DIV') event.target.classList.toggle("pop-out");
+let thumbnails = document.getElementsByClassName('thumbnail');
+
+for (let i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].addEventListener('click', function(){
+        if (i === thumbnails.length-1) {
+            thumbnails[i].classList.toggle('expand-width');
+            thumbnails[i].classList.toggle('expand-height');
+        } else {
+            thumbnails[i].classList.toggle((i+1)%2 == 0 ? 'expand-width' : 'expand-height');
+        }
+    });
 }
